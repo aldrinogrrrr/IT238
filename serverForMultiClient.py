dbb1f0d49418e71124560c57dc5a568163675de6
Aldrin <137375592+aldrinogrrrr@users.noreply.github.com>
2023-10-05 14:39:16 +0800

    Update serverForMultiClient.py

--- serverForMultiClient.py
+++ serverForMultiClient.py
@@ -1,14 +1,14 @@
 import socket
 
-# Server configuration
+
 SERVER_HOST = '0.0.0.0'  # Listen on all available interfaces
 SERVER_PORT = 12345
 
-# Create a UDP socket
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((SERVER_HOST, SERVER_PORT))
 
-# Store client addresses and names
+
 clients = {}
 
 while True:
@@ -16,7 +16,7 @@ while True:
     message = data.decode()
 
     if client_address not in clients:
-        # New client, ask for their name
+
         server_socket.sendto("Please enter your name: ".encode(), client_address)
         name_data, _ = server_socket.recvfrom(1024)
         name = name_data.decode()
@@ -24,7 +24,7 @@ while True:
         broadcast(f"Welcome {name}!", exclude=[client_address])
 
     else:
-        # Existing client, broadcast their message to others
+
         sender_name = clients[client_address]
         print(f"{sender_name}: {message}")
         broadcast(f"{sender_name}: {message}", exclude=[client_address])
