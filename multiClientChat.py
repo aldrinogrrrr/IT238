8b95d8e2fcc642b6685f103f3c2b7cbd62d6ab75
aldrinogerr <137375592+aldrinogrrrr@users.noreply.github.com>
2023-10-05 14:02:44 +0800

    Pushing code changes to Main

--- multiClientChat.py
+++ multiClientChat.py
@@ -1,14 +1,17 @@
 import socket
 
+# Server configuration
 SERVER_IP = 'your_server_ip'  # Replace with your server's IP address
 SERVER_PORT = 12345
 
+# Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    message = input("Enter a message: ")
+    message = input("Enter a message to send (or 'q' to quit): ")
     if message.lower() == 'q':
         break
     client_socket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
 
+# Close the socket when done
 client_socket.close()
--- serverForMultiClient.py
+++ serverForMultiClient.py
@@ -1,36 +1,33 @@
 import socket
 import threading
 
-SERVER_HOST = '0.0.0.0'
+# Server configuration
+SERVER_HOST = '0.0.0.0'  # Listen on all available interfaces
 SERVER_PORT = 12345
 
+# Create a UDP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((SERVER_HOST, SERVER_PORT))
 
-
+# Store client addresses
 client_addresses = set()
 
 def listen_for_messages():
     while True:
         data, client_address = server_socket.recvfrom(1024)
         print(f"Received message from {client_address}: {data.decode()}")
-
-def broadcast_message():
-    while True:
-        message = input("Enter a message: ")
-        for client_address in client_addresses:
-            server_socket.sendto(message.encode(), client_address)
+        # Broadcast the message to all connected clients
+        for address in client_addresses:
+            if address != client_address:
+                server_socket.sendto(data, address)
 
 if __name__ == "__main__":
     print(f"Server is listening on {SERVER_HOST}:{SERVER_PORT}")
 
+    # Start a thread to listen for incoming messages
     message_listener_thread = threading.Thread(target=listen_for_messages)
     message_listener_thread.daemon = True
     message_listener_thread.start()
-
-    broadcast_thread = threading.Thread(target=broadcast_message)
-    broadcast_thread.daemon = True
-    broadcast_thread.start()
 
     try:
         while True:
