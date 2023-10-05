dbc570e766d06b35cad36c3a3daa251924ff7d58
aldrinogerr <137375592+aldrinogrrrr@users.noreply.github.com>
2023-10-05 11:36:47 +0800

    Pushing code changes to Main

--- chatClient.py
+++ chatClient.py
@@ -1,30 +1,34 @@
 import socket
-import keyboard
+import threading
 
-#11th version
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def receive_messages(client_socket):
+    while True:
+        response = client_socket.recv(1024).decode('utf-8')
+        print(response)
+
 server_ip = '192.168.1.35'
 server_port = 9999
 
 try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((server_ip, server_port))
     print(f"Connected to the server at {server_ip}:{server_port}")
 
+    # Start a thread to continuously receive messages from the server
+    receive_thread = threading.Thread(target=receive_messages, args=(client,))
+    receive_thread.start()
+
     while True:
         message = input("Enter your message (or ctrl + x to quit): ")
-        client.send(message.encode('utf-8'))
         if message.lower() == 'exit':
             break
 
-        response = client.recv(1024).decode('utf-8')
-        print(response)
-
-        if keyboard.is_pressed('ctrl') and keyboard.is_pressed('x'):
-            break
+        client.send(message.encode('utf-8'))
 
 except ConnectionRefusedError:
     print(f"Connection to {server_ip}:{server_port} was refused. Make sure the server is running.")
 except Exception as e:
     print(f"An error occurred: {e}")
 
-client.close()
+finally:
+    client.close()
