e9790f7f458922e80ac8eee7a481b948b39fd9ff
aldrinogerr <137375592+aldrinogrrrr@users.noreply.github.com>
2023-10-06 12:29:31 +0800

    Pushing code changes to Main

--- multiClientChat.py
+++ multiClientChat.py
@@ -1,18 +1,27 @@
 import socket
+import threading
 
+# Server settings
+server_ip = "YOUR_SERVER_IP"  # Replace with the actual server IP
+server_port = 12345  # Replace with the desired port number
 
-SERVER_IP = 'your_server_ip'  # Replace with your server's IP address
-SERVER_PORT = 12345
-
-
+# Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Register a name with the server
+name = input("Enter your name: ")
+client_socket.sendto(name.encode('utf-8'), (server_ip, server_port))
+
+def receive_messages():
+    while True:
+        message, _ = client_socket.recvfrom(1024)
+        print(message.decode('utf-8'))
+
+# Start a thread to receive messages
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start()
+
+# Send messages to the server and other clients
 while True:
-    message = input("Enter a message to send (or 'q' to quit): ")
-    if message.lower() == 'q':
-        break
-
-    client_socket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
-
-
-client_socket.close()
+    message = input()
+    client_socket.sendto(message.encode('utf-8'), (server_ip, server_port))
--- serverForMultiClient.py
+++ serverForMultiClient.py
@@ -1,35 +1,35 @@
 import socket
+import threading
 
+# Server settings
+server_ip = "YOUR_SERVER_IP"  # Replace with the actual server IP
+server_port = 12345  # Replace with the desired port number
 
-SERVER_HOST = '0.0.0.0'  # Listen on all available interfaces
-SERVER_PORT = 12345
-
-
+# Create a UDP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.bind((server_ip, server_port))
 
+# Store client addresses and their names
+client_info = {}
 
-clients = {}
+print("Server is running...")
+
+def broadcast(message, sender_name):
+    chat_message = f"{sender_name}: {message}"
+    for addr in client_info:
+        if addr != sender_name:
+            server_socket.sendto(chat_message.encode('utf-8'), addr)
 
 while True:
     data, client_address = server_socket.recvfrom(1024)
-    message = data.decode()
+    data = data.decode('utf-8')
 
-    if client_address not in clients:
-
-        server_socket.sendto("Please enter your name: ".encode(), client_address)
-        name_data, _ = server_socket.recvfrom(1024)
-        name = name_data.decode()
-        clients[client_address] = name
-        broadcast(f"Welcome {name}!", exclude=[client_address])
-
+    if client_address not in client_info:
+        # Register new client
+        client_info[client_address] = data
+        print(f"Client '{data}' connected from {client_address}")
+        welcome_message = f"Server: Welcome {data}! You can start chatting now."
+        server_socket.sendto(welcome_message.encode('utf-8'), client_address)
     else:
-
-        sender_name = clients[client_address]
-        print(f"{sender_name}: {message}")
-        broadcast(f"{sender_name}: {message}", exclude=[client_address])
-
-def broadcast(message, exclude=[]):
-    for client_address in clients.keys():
-        if client_address not in exclude:
-            server_socket.sendto(message.encode(), client_address)
+        # Broadcast message to all clients
+        broadcast(data, client_info[client_address])
