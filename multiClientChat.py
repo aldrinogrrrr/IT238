a17a50ee644d35c324167b51b680b14ae834aa87
aldrinogerr <137375592+aldrinogrrrr@users.noreply.github.com>
2023-10-06 12:31:23 +0800

    Pushing code changes to Main

--- multiClientChat.py
+++ multiClientChat.py
@@ -2,13 +2,13 @@ import socket
 import threading
 
 # Server settings
-server_ip = "YOUR_SERVER_IP"  # Replace with the actual server IP
-server_port = 12345  # Replace with the desired port number
+server_ip = "YOUR_SERVER_IP"
+server_port = 12345
 
-# Create a UDP socket
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Register a name with the server
+
 name = input("Enter your name: ")
 client_socket.sendto(name.encode('utf-8'), (server_ip, server_port))
 
@@ -17,11 +17,11 @@ def receive_messages():
         message, _ = client_socket.recvfrom(1024)
         print(message.decode('utf-8'))
 
-# Start a thread to receive messages
+
 receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
 
-# Send messages to the server and other clients
+
 while True:
     message = input()
     client_socket.sendto(message.encode('utf-8'), (server_ip, server_port))
--- serverForMultiClient.py
+++ serverForMultiClient.py
@@ -1,15 +1,15 @@
 import socket
 import threading
 
-# Server settings
-server_ip = "YOUR_SERVER_IP"  # Replace with the actual server IP
-server_port = 12345  # Replace with the desired port number
 
-# Create a UDP socket
+server_ip = "YOUR_SERVER_IP"
+server_port = 12345
+
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((server_ip, server_port))
 
-# Store client addresses and their names
+
 client_info = {}
 
 print("Server is running...")
@@ -25,11 +25,9 @@ while True:
     data = data.decode('utf-8')
 
     if client_address not in client_info:
-        # Register new client
         client_info[client_address] = data
         print(f"Client '{data}' connected from {client_address}")
         welcome_message = f"Server: Welcome {data}! You can start chatting now."
         server_socket.sendto(welcome_message.encode('utf-8'), client_address)
     else:
-        # Broadcast message to all clients
         broadcast(data, client_info[client_address])
