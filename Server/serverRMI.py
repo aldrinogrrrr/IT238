177026d62e2699d16349d74f9617ba9db5148c7f
aldrinogerr <137375592+aldrinogrrrr@users.noreply.github.com>
2023-10-17 11:59:54 +0800

    Pushing code changes

--- Server/serverRMI.py
+++ Server/serverRMI.py
@@ -13,8 +13,8 @@ print("Server is up and running...")
 
 def broadcast_msg(msg, sender):
     chat_msg = f"{sender}: {msg}"
-    for clientLocation in clientList:
-        serverSocket.sendto(chat_msg.encode('utf-8'), clientLocation)
+    for clientAddress in clientList:  # Changed the variable name for clarity
+        serverSocket.sendto(chat_msg.encode('utf-8'), clientAddress)
 
 while True:
     clientName, clientAddress = serverSocket.recvfrom(1024)
@@ -28,6 +28,7 @@ while True:
             serverSocket.sendto(welcomeMessage.encode('utf-8'), addr)
     else:
         broadcast_msg(clientName, clientList[clientAddress])
+
 
 
 # Initialize Pyro5 Client
