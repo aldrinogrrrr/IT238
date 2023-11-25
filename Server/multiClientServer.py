461225baf0161a9d92887ab58fc37ddf58f2980a
falabastro <f.alabastro@dxc.com>
2023-11-25 18:35:45 +0800

    Initial commit to IT238

--- Server/multiClientServer.py
+++ Server/multiClientServer.py
@@ -9,10 +9,12 @@ clientList = {}
 
 print("Server is up and running...")
 
+
 def broadcast_msg(msg, sender):
     chat_msg = f"{sender}: {msg}"
     for clientLocation in clientList:
         serverSocket.sendto(chat_msg.encode('utf-8'), clientLocation)
+
 
 while True:
     clientName, clientAddress = serverSocket.recvfrom(1024)
